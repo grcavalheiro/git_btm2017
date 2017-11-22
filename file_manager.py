3cf1adf11cb4ac932b7494378e12004ef60a5bbe
phil182 <p.hoess@arcor.de>
2017-11-22 15:14:21 +0000

    Added add_cli[Bent function"

--- file_manager.py
+++ file_manager.py
@@ -4,11 +4,12 @@ client_filename = "clients.txt"
 transaction_filename = "transactions.txt"
 
 def add_client(client_name):
-    pass
-      
+    with open(client_filename,'a') as file:
+        file.write(client_name+"\n")
+
 def add_transaction(debtor, creditor, amount):
     pass
-        
+
 def get_clients():
     pass
 
